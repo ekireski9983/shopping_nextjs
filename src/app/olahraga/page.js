ab594c380e5ae26b27b6fdeb3d1665b9608466ea
ekireski9983 <rezkyjayadis@gmail.com>
2024-09-08 22:34:50 +0700

    rename file

--- src/app/olahraga/page.js
+++ src/app/olahraga/page.js
@@ -1,4 +1,4 @@
-export default function shopping (){
+export default function olahraga (){
     return(
       <>belanja</>
     );
--- src/app/smartphone/page.js
+++ src/app/smartphone/page.js
@@ -1,4 +1,4 @@
-export default function login (){
+export default function smartphone (){
     return(
         <></>
     );
