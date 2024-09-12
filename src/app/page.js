7d968a4cdca4b10004a51a18b35361bb44b68d09
ekireski9983 <rezkyjayadis@gmail.com>
2024-09-12 09:51:59 +0700

    tambahkan button

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ export default function Home (){
     <section class="bg-white py-12 text-gray-700 sm:py-16 lg:py-20">
   <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
     <div class="mx-auto max-w-md text-center">
-      <h2 class="font-serif text-2xl font-bold sm:text-3xl">Our featured Aroma Range</h2>
-      <p class="mt-4 text-base text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus faucibus massa dignissim tempus.</p>
+      <h2 class="font-serif text-2xl font-bold sm:text-3xl">selamat datang di shopping nextjs</h2>
+      <p class="mt-4 text-base text-gray-700">silahkan dibeli jangan lupa di checkout</p>
     </div>
 
     <div class="mt-10 grid grid-cols-2 gap-6 lg:mt-16 lg:grid-cols-4 lg:gap-4">
@@ -39,6 +39,9 @@ export default function Home (){
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" class=""></path>
               </svg>
             </div>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+               beli
+              </button>
           </div>
 
           <div class="text-right">
@@ -77,6 +80,9 @@ export default function Home (){
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" class=""></path>
               </svg>
             </div>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+               beli
+              </button>
           </div>
 
           <div class="text-right">
@@ -93,7 +99,7 @@ export default function Home (){
           <div class="">
             <h3 class="text-xs font-semibold sm:text-sm md:text-base">
               <a href="#" title="" class="cursor-pointer">
-              Baju Kaos Pria Lengan Pendek Atasan Cowok Tshirt Taurus Fashion Distro - Hijau botol, XXL
+              Baju Kaos Pria Lengan Pendek Atasan 
                 <span class="absolute" aria-hidden="true"></span>
               </a>
             </h3>
@@ -114,6 +120,9 @@ export default function Home (){
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" class=""></path>
               </svg>
             </div>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+               beli
+              </button>
           </div>
 
           <div class="text-right">
@@ -130,7 +139,7 @@ export default function Home (){
           <div class="">
             <h3 class="text-xs font-semibold sm:text-sm md:text-base">
               <a href="#" title="" class="cursor-pointer">
-              Baju Kaos Pria Lengan Pendek Atasan Cowok Tshirt Taurus Fashion Distro - Hijau botol, XL
+              Baju Kaos Pria Lengan Pendek Atasan 
                 <span class="absolute" aria-hidden="true"></span>
               </a>
             </h3>
@@ -151,6 +160,9 @@ export default function Home (){
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" class=""></path>
               </svg>
             </div>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+               beli
+              </button>
           </div>
 
           <div class="text-right">
@@ -190,6 +202,9 @@ export default function Home (){
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" class=""></path>
               </svg>
             </div>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+               beli
+              </button>
           </div>
 
           <div class="text-right">
